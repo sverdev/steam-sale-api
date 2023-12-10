a19bece9c66e80eebea101e16df73d464275662a
Jihyuk Lee <jihyuk1714@gmail.com>
2023-12-10 17:03:50 +0900

    edit: upscale image

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
-import unicodedata
+import unicodedata, re
 
 
 def delete_previous_data():
@@ -40,13 +40,23 @@ def crawl_and_save_discounts():
         title = game.find('span', class_='title').text.strip()
         release_date = game.find('div', class_='search_released').text.strip()
         discount_price = game.find('div', class_='discount_final_price').text.strip().replace("₩ ", "")
-        discount_pct = game.find('div', class_='discount_pct').text.strip()
+        discount_original_price = game.find('div', class_='discount_original_price').text.strip()
         image_url = game.find('div', class_='search_capsule').find('img')['src']
         game_url = game['href']
         
+            # Use regular expression to extract the unique ID from the URL
+        match = re.search(r"/apps/(\d+)/", image_url)
+        if match:
+            unique_id = match.group(1)
+            image_url = f'https://cdn.cloudflare.steamstatic.com/steam/apps/{unique_id}/header.jpg'
+        else:
+            unique_id = None  # You can set it to a default value or handle it as needed
+
+
+        
         # Insert the data into the database
-        cursor.execute("INSERT INTO game_sales (title, release_date, discount_price, discount_pct, image_url, game_url) VALUES (?, ?, ?, ?, ?, ?)",
-                   (title, release_date, discount_price, discount_pct, image_url, game_url))
+        cursor.execute("INSERT INTO game_sales (title, release_date, discount_price, discount_original_price, image_url, game_url) VALUES (?, ?, ?, ?, ?, ?)",
+                   (title, release_date, discount_price, discount_original_price, image_url, game_url))
 
     # Commit and close the connection
     conn.commit()
